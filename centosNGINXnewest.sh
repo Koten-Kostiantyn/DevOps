325c83d4d43297231d6303b443c451f9529f9c6a
Kostiantyn <kotkotkot@i.ua>
2015-12-26 20:47:59 +0200

    Create centosNGINXnewest.sh

---
+++ centosNGINXnewest.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#CentOS only
+# Am I root?
+if [ "x$(id -u)" != 'x0' ]; then
+    echo 'Error: this script can only be executed by root'
+    exit 1
+fi
+
+# Check admin user account
+if [ ! -z "$(grep ^admin: /etc/passwd)" ] && [ -z "$1" ]; then
+    echo "Error: user admin exists"
+    echo
+    echo 'Please remove admin user account before proceeding.'
+    echo 'If you want to do it automatically run installer with -f option:'
+    echo "Example: bash $0 --force"
+    exit 1
+fi
+
+# Check admin user account
+if [ ! -z "$(grep ^admin: /etc/group)" ] && [ -z "$1" ]; then
+    echo "Error: group admin exists"
+    echo
+    echo 'Please remove admin user account before proceeding.'
+    echo 'If you want to do it automatically run installer with -f option:'
+    echo "Example: bash $0 --force"
+    exit 1
+fi
+
+# Detect OS
+case $(head -n1 /etc/issue | cut -f 1 -d ' ') in
+    Debian)     echo "Error: wrong OS";exit 1 ;;
+    Ubuntu)     echo "Error: wrong OS";exit 1 ;;
+    *)          type="rhel" ;;
+esac
+
+cat > /etc/yum.repos.d/nginx.repo<<EOF
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/mainline/centos/$releasever/$basearch/
+gpgcheck=0
+enabled=1
+EOF
+yum install nginx
