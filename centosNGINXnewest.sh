7c330ad5305052a969a63fcde3c69175616451ea
Kostiantyn <kotkotkot@i.ua>
2015-12-27 14:42:54 +0200

    Update centosNGINXnewest.sh

--- centosNGINXnewest.sh
+++ centosNGINXnewest.sh
@@ -36,7 +36,7 @@ esac
 cat > /etc/yum.repos.d/nginx.repo<<EOF
 [nginx]
 name=nginx repo
-baseurl=http://nginx.org/packages/mainline/centos/$releasever/$basearch/
+baseurl=http://nginx.org/packages/mainline/centos/\$releasever/\$basearch/
 gpgcheck=0
 enabled=1
 EOF
