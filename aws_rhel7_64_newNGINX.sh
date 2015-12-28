365607bdb36a043ae63b8e89419dd17115a017ac
Kostiantyn <kotkotkot@i.ua>
2015-12-28 11:34:04 +0200

    Update and rename centosNGINXnewest.sh to aws_rhel7_64_newNGINX.sh
    aws do not understand $basearch && $releasever

--- aws_rhel7_64_newNGINX.sh
+++ aws_rhel7_64_newNGINX.sh
@@ -36,7 +36,7 @@ esac
 cat > /etc/yum.repos.d/nginx.repo<<EOF
 [nginx]
 name=nginx repo
-baseurl=http://nginx.org/packages/mainline/centos/\$releasever/\$basearch/
+baseurl=http://nginx.org/packages/mainline/centos/7/x86_64/repodata/
 gpgcheck=0
 enabled=1
 EOF
