7bb225edbd0ca375118b338c596ac51818f9480f
Kostiantyn <kotkotkot@i.ua>
2016-06-08 14:02:33 +0300

    Create pp.sh

---
+++ pp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ssh -o "StrictHostKeyChecking no" -i oregon.pem ec2-user@$1 << EOF
+sudo su
+sed -i '/PermitRootLogin/d' /etc/ssh/sshd_config 
+sed -i '/PasswordAuthentication/d' /etc/ssh/sshd_config 
+
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+
+echo -e "1" | (passwd --stdin $USER)
+service sshd restart
+
+EOF
+# sed -i 's/PermitRootLogin/#PermitRootLogin/g' /etc/ssh/sshd_config
+# sed -i 's/PasswordAuthentication/#PasswordAuthentication/g' /etc/ssh/sshd_config
