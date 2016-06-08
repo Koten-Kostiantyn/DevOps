7002d5074e087dd1fb2c21ba6d51d90b718555e5
Kostiantyn <kotkotkot@i.ua>
2016-06-08 14:31:40 +0300

    Update pp.sh

--- pp.sh
+++ pp.sh
@@ -12,5 +12,11 @@ echo -e "1" | (passwd --stdin $USER)
 service sshd restart
 
 EOF
+
+ssh -o "StrictHostKeyChecking no" -i oregon.pem ec2-user@$1 << HERE
+sudo su
+service puppetmaster start
+HERE
+
 # sed -i 's/PermitRootLogin/#PermitRootLogin/g' /etc/ssh/sshd_config
 # sed -i 's/PasswordAuthentication/#PasswordAuthentication/g' /etc/ssh/sshd_config
